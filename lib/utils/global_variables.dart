c425abf18b1c813b7bb1df6bcb01b214644d5df9
Murali Krishnan MT <mtmuralikrishnan@gmail.com>
2023-03-14 14:43:59 +0530

    Search screen added

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instogram/screens/add_post_screen.dart';
 import 'package:instogram/screens/feed_screen.dart';
+import 'package:instogram/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('notif'),
   Text('profile'),
